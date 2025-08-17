8a03dd7a1a2ae697b394572142f8006a8d076b62
MayB-04 <maybadenhorst@gmail.com>
2025-08-17 10:30:23 +0200

    feat(landing page): Adding google dign in button to landing page

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
 import { Topbar } from './shared/components/topbar/topbar';
+import { AdminDashboard } from "./dashboard/modules/admin-dashboard/admin-dashboard";
+import { ButtonGoogle } from './shared/components/button/modules/button-google/button-google';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Topbar],
+  imports: [RouterOutlet, Navbar, Topbar, AdminDashboard, ButtonGoogle],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
