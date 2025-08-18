4cc6f9fc76371cb762386756285c55fe0434554d
DylanDaviesABI <dylan@automationbi.co.za>
2025-08-18 12:31:56 +0200

    feat: update deploy for non-ssr and test mode

--- src/app/shared/components/button/modules/button-google/button-google.ts
+++ src/app/shared/components/button/modules/button-google/button-google.ts
@@ -3,7 +3,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 
-import { environment } from "../../../../../environments/environment";
+import { environment } from "../../../../../../environments/environment";
 
 @Component({
   selector: 'app-button-google',
