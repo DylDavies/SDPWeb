1312954c79ea6186511ef259f4340f32da7dfceb
MichaelHB04 <michael.h.badenhorst@gmail.com>
2025-08-19 14:25:12 +0200

    adding a card to the profile page

--- src/app/dashboard/modules/profile-dashboard/profile-dashboard.ts
+++ src/app/dashboard/modules/profile-dashboard/profile-dashboard.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Card } from '../../../shared/components/card/card';
+import { CommonModule } from '@angular/common';
+import { CardAction } from '../../../models/interfaces/ICardAction.Interface';
+import { AuthService } from '../../../services/auth-service';
+import { IUser } from '../../../models/interfaces/IUser.interface';
 
 @Component({
-  selector: 'app-profile',
-  imports: [],
+  selector: 'app-profile-dashboard',
+  standalone: true,
+  imports: [CommonModule,  Card  ],
   templateUrl: './profile-dashboard.html',
   styleUrl: './profile-dashboard.scss'
 })
-export class Profile {
+export class Profile implements OnInit {
+  
+  public authService = inject(AuthService);
+  public user: IUser | null = null;
 
+  profileCardActions: CardAction[] = [];
+
+  ngOnInit(): void {
+    // Subscribe to the currentUser observable to get user data
+    this.authService.currentUser$.subscribe({
+      next: (user) => {
+        this.user = user;
+      }
+    });
+  }
 }
