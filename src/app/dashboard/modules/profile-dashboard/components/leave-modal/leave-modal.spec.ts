944d7f79695200d90b1e12a4481c341682be8f2a
Sudhir Ajodhya <62798738+SudhirAjodhya@users.noreply.github.com>
2025-08-25 12:36:00 +0200

    chore: cleaned up leave-modal

--- src/app/dashboard/modules/profile-dashboard/components/leave-modal/leave-modal.spec.ts
+++ src/app/dashboard/modules/profile-dashboard/components/leave-modal/leave-modal.spec.ts
@@ -8,12 +8,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./leave-modal.scss']
 })
 export class LeaveModal {
-
-  // Creates an event that the parent component can listen for.
-  //@Output() close = new EventEmitter<void>();
-
-  // This is the function that the HTML calls to close the modal.
-  // onClose(): void {
-  //   this.close.emit(); // This sends the "close" signal to the parent.
-  // }
 }
